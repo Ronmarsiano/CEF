21deede4429a16264effed6c27959620f1ed616c
Ron Marsiano <romarsia@microsoft.com>
2019-07-07 14:51:32 +0300

    update installer

--- cef_installer.py
+++ cef_installer.py
@@ -240,7 +240,7 @@ def set_rsyslog_configuration():
         insert_to_file(file_path=rsyslog_conf_path,
                        string_to_append="\"module(load=\"imudp\")\ninput(type=\"imudp\" port=\"" + daemon_default_incoming_port + "\")\n\"")
     if not tcp_enabled:
-        print_error("UPDATINNGGGGGGGGGGGGGGGGGGGGG123")
+        print_error("UPDATINNGGGGGGGGGGGGGGGGGGGGG123  ")
         insert_to_file(file_path=rsyslog_conf_path,
                        string_to_append="\"module(load=\"imtcp\")\ninput(type=\"imtcp\" port=\"" + daemon_default_incoming_port + "\")\n\"")
     print_ok("Omsagent configuration was changed to fit required protocol - " + rsyslog_conf_path)
