5ad8e24bcefbb49835a9b8fa48781af1aae5862b
Ronmarsiano <ronmarsiano@gmail.com>
2019-09-11 10:47:57 +0300

    Change files

--- cef_installer.py
+++ cef_installer.py
@@ -47,17 +47,29 @@ rsyslog_old_config_tcp_content = "# provides TCP syslog reception\n$ModLoad imtc
 oms_agent_configuration_url = "https://raw.githubusercontent.com/microsoft/OMS-Agent-for-Linux/master/installer/conf/omsagent.d/security_events.conf"
 
 
-def replace_files():
+def replace_filter_syslog_security():
     download_command = subprocess.Popen(["sudo", "wget", "-O", "/opt/microsoft/omsagent/plugin/filter_syslog_security.rb", "https://raw.githubusercontent.com/microsoft/OMS-Agent-for-Linux/master/source/code/plugins/filter_syslog_security.rb"], stdout=subprocess.PIPE)
     o, e = download_command.communicate()
     time.sleep(3)
     if e is not None:
-        handle_error(e, error_response_str="Error: could not change omsagent files.")
+        handle_error(e, error_response_str="Error: could not change filter_syslog_security.")
+    else:
+        print_ok("Replaced filter_syslog_security")
+
+
+def replace_security_lib():
     download_command = subprocess.Popen(["sudo", "wget", "-O", "/opt/microsoft/omsagent/plugin/security_lib.rb", "https://raw.githubusercontent.com/microsoft/OMS-Agent-for-Linux/master/source/code/plugins/security_lib.rb"], stdout=subprocess.PIPE)
     o, e = download_command.communicate()
     time.sleep(3)
     if e is not None:
-        handle_error(e, error_response_str="Error: could not change omsagent files.")
+        handle_error(e, error_response_str="Error: could not change security_lib.")
+    else:
+        print_ok("Replaced security_lib")
+
+
+def replace_files():
+    replace_filter_syslog_security()
+    replace_security_lib()
 
 
 def print_error(input_str):
@@ -512,10 +524,9 @@ def main():
                                                daemon_name=syslog_ng_daemon_name)
         set_syslog_ng_configuration()
         restart_syslog_ng()
-    restart_omsagent(workspace_id=workspace_id)
     replace_files()
+    restart_omsagent(workspace_id=workspace_id)
     print_ok("Installation completed")
 
 
 main()
-
