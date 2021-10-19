077e84efaf7decfe301d0008e15e23fc6415f3ee
Ron Marsiano <ronmarsiano@gmail.com>
2021-10-19 08:49:45 +0300

    working on blog post

--- detection_tuning_blog_post.py
+++ detection_tuning_blog_post.py
@@ -6,7 +6,7 @@ import random
 
 
 fixed_message_p1 = "0|Test "
-fixed_message_p2 = "|PAN-OS|common=event-format-test|end|TRAFFIC|1|deviceExternalId=0002D01655 dpt=49514 app=incomplete reason=tcp-rst-from-server act=deny src=1.1.1.1 dst=4.4.4.4 sourceTranslatedAddress=1.1.1.1 destinationTranslatedAddress=3.3.3.3 msg=Account has been deleted AccountFullName=Harley.Quinn DisplayName=HarleenFrances Quinzel"
+fixed_message_p2 = "|PAN-OS|common=event-format-test|end|TRAFFIC|1|deviceExternalId=0002D01655 dpt=49514 app=incomplete reason=tcp-rst-from-server act=deny src=1.1.1.1 dst=4.4.4.4 sourceTranslatedAddress=1.1.1.1 destinationTranslatedAddress=3.3.3.3"
 cisco_message = "Inbound TCP connection denied from 183.60.23.164/58098 to 131.107.193.171/23 flags SYN  on interface inet"
 
 
