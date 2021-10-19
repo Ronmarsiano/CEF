9418aa2d9f4b8aac933f04bf71c747ae42d6b11c
Ron Marsiano <ronmarsiano@gmail.com>
2021-10-19 09:01:26 +0300

    update

--- detection_tuning_blog_post.py
+++ detection_tuning_blog_post.py
@@ -6,7 +6,7 @@ import random
 
 
 fixed_message_p1 = "0|Palo Alto Networks"
-fixed_message_p2 = "|PAN-OS|common=event-format-test|end|TRAFFIC|1|deviceExternalId=0002D01655 dpt=49514 dvchost=PaloAltoDevice app=incomplete reason=tcp-rst-from-server act=deny src=192.168.0.1 dst=164.121.0.4 sourceTranslatedAddress=1.1.1.1 destinationTranslatedAddress=3.3.3.3 testIndex="
+fixed_message_p2 = "|PAN-OS|common=event-format-test|end|TRAFFIC|1|deviceExternalId=0002D01655 dpt=49514 proto=TCP dvchost=PaloAltoDevice app=incomplete reason=tcp-rst-from-server act=deny src=192.168.0.1 dst=164.121.0.4 sourceTranslatedAddress=1.1.1.1 destinationTranslatedAddress=3.3.3.3 testIndex="
 cisco_message = "Inbound TCP connection denied from 183.60.23.164/58098 to 131.107.193.171/23 flags SYN  on interface inet"
 
 
